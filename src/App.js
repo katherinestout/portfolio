3918701ec6754c548d439a43d24244f1a910070a
katherinestout <kjs14@hood.edu>
2018-10-30 16:31:10 -0400

    about

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
 import React, { Component } from 'react';
+import Header from './components/Header';
+import About from './components/About';
+//import PortfolioPeices from './components/PortfolioPeices';
+//import Skills from './components/Skills';
+import Footer from './components/Footer';
+
 
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-    
+      
+      <Header/>
+      <About/>
+      <Footer/>
       </div>
     );
   }
---
+++ src/components/About.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+
+class About extends Component {
+  render() {
+    return (
+      <div className="About">
+      <h1>Get to know me:</h1>
+    <p>In 2017 I graduated from Hood College, 
+        with a degree in Communications and a Minor in Studio Art.
+        During my college years, I discovered two of my greatest passions;
+        photography and coding. From 2016 - present I have ran my own photography 
+        business.
+
+        While I have dappled in coding for a few years, I finally took the plunge to 
+        advance my career in 2018 when I earned my Full Stack Certificate from 
+        George Washington University. 
+        </p>
+      </div>
+    );
+  }
+}
+
+export default About;
