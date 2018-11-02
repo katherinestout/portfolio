d45d2c9cd4e84186e5c7ed6ee052c64f05106c00
katherinestout <kjs14@hood.edu>
2018-11-01 20:34:32 -0400

    screenshots

--- src/components/PortfolioPeices.js
+++ src/components/PortfolioPeices.js
@@ -1,5 +1,9 @@
 import React, { Component } from 'react';
 import  "./style/portfolio.css";
+import clevercap from "./screenshots/clevercap.png";
+import crystal from "./screenshots/crystal.png";
+import drinkerator from "./screenshots/drinkerator.png";
+import todo from "./screenshots/todolist.png";
 
 class PortfolioPeices extends Component {
   render() {
@@ -7,6 +11,12 @@ class PortfolioPeices extends Component {
       <div className="Portfolio">
       <h2> Projects </h2>
       <div>hi this is where portfolio p goes</div>
+      <div className="projects">
+        <img src={clevercap} alt="clevercap" className="screenshots" ></img>
+        <img src={crystal} alt="crystal" className="screenshots" ></img>
+        <img src={drinkerator} alt="drinkerator" className="screenshots" ></img>
+        <img src={todo} alt="todo" className="screenshots" ></img>
+      </div>
       </div>
     );
   }
