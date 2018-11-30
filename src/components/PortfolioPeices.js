4e696c922d2dfe11329b2112b3f43abe81b94c80
katherinestout <kjs14@hood.edu>
2018-11-30 16:29:33 -0500

    project planner

--- src/components/PortfolioPeices.js
+++ src/components/PortfolioPeices.js
@@ -5,6 +5,7 @@ import crystal from "./screenshots/crystal.png";
 import drinkerator from "./screenshots/drinkerator.png";
 import todo from "./screenshots/todolist.png";
 import wordguess from "./screenshots/wordguess.png";
+import projectPlanner from "./screenshots/ProjectPlanner.png";
 
 class PortfolioPeices extends Component {
   render() {
@@ -32,6 +33,12 @@ class PortfolioPeices extends Component {
         <a href ="https://katherinestout.github.io/PoliticsGame/">
         <img src={wordguess} alt="wordguess" className="screenshots"></img>
         </a>
+
+          <a href ="https://katprojectplan.firebaseapp.com/signin ">
+        <img src={projectPlanner} alt="projectPlanner" className="screenshots"></img>
+        </a>
+
+
 
 
       </div>
