de06afbcb5d724559cfdc1ecbb5960e62c388ab2
katherinestout <kjs14@hood.edu>
2018-12-15 16:38:19 -0500

    Updates

---
+++ precache-manifest.330754e7e55447aba61625e2b17dfeac.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "1dacb122d4fa2930fb4ad29de210a24f",
+    "url": "/portfolio/static/media/crystal.1dacb122.png"
+  },
+  {
+    "revision": "042a4ba97833b93f9a93",
+    "url": "/portfolio/static/css/main.f5eab764.chunk.css"
+  },
+  {
+    "revision": "64d227e355982ad7c2fd",
+    "url": "/portfolio/static/js/1.64d227e3.chunk.js"
+  },
+  {
+    "revision": "ee2a100325fd284f0097",
+    "url": "/portfolio/static/js/runtime~main.ee2a1003.js"
+  },
+  {
+    "revision": "dffde38ae4140c3012efe981a3958588",
+    "url": "/portfolio/static/media/clevercap.dffde38a.png"
+  },
+  {
+    "revision": "042a4ba97833b93f9a93",
+    "url": "/portfolio/static/js/main.042a4ba9.chunk.js"
+  },
+  {
+    "revision": "c64e06772a15fdeb52968ffca16a5172",
+    "url": "/portfolio/static/media/drinkerator.c64e0677.png"
+  },
+  {
+    "revision": "27e0ede7ff84d0eddbef24ac95fbd82f",
+    "url": "/portfolio/static/media/todolist.27e0ede7.png"
+  },
+  {
+    "revision": "765cb9170cef84465df9fb37a38e8d24",
+    "url": "/portfolio/static/media/wordguess.765cb917.png"
+  },
+  {
+    "revision": "523533498f0b10380b44f4420c0ab15e",
+    "url": "/portfolio/static/media/ProjectPlanner.52353349.png"
+  },
+  {
+    "revision": "85e89b13d34429ff9793a0b16eb935c5",
+    "url": "/portfolio/index.html"
+  }
+];
