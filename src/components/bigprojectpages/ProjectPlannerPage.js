678c91920aa8b7b3a4fd398100de9e93315918de
katherinestout <kjs14@hood.edu>
2018-12-20 15:27:41 -0500

    big project pages set up

--- src/components/bigprojectpages/ProjectPlannerPage.js
+++ src/components/bigprojectpages/ProjectPlannerPage.js
@@ -6,11 +6,21 @@ class ProjectPlannerPage extends Component {
   render() {
     return (
 
-<div className="page">
+<div>
 
 <ProjectLinks/>
-
+<div className="page">
 <h1>ProjectPlanner </h1>
+
+<div className="projectContent">
+<h2>Planning</h2>
+<h3>Problem to be solved</h3>
+<h3>Persona</h3>
+<h2>Design</h2>
+<h2>Finished Product</h2>
+<h3>How it works</h3>
+</div>
+      </div>
       </div>
 
     );
